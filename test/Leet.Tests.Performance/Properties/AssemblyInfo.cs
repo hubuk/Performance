dd3d0ce3a4bc13d53d1c1d7482b9a166aa7766df
Hubert Bukowski <hubert.bukowski@gmail.com>
2017-02-06 20:08:25 +0100

    Initialized solution and projects.

---
+++ test/Leet.Tests.Performance/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Leet">
+//     Copyright (c) Leet. All rights reserved.
+//     Licensed under the MIT License.
+//     See License.txt in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Leet")]
+[assembly: AssemblyProduct("Performance")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
